7a1a09051735d044e215b7d2af969faab2351e65
张某0722 <827149819@qq.com>
2021-07-20 11:24:42 +0800

    '初始项目目录结构'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+/**
+ * 项目的启动入口
+ * */
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
+// 创建 Vue 根实例
+// 把router配置到根实例中
+// 通过render方法把App根组件渲染到 #app 入口节点
 new Vue({
   router,
   render: h => h(App)
+  // el:‘#app’ 等价于 $mount('#app')
 }).$mount('#app')
